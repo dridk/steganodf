80359fea2b7f6255c548d0933b485b441f70f2de
dridk <sacha@labsquare.org>
2023-12-29 00:30:37 +0100

    add parametrize test for error count

--- tests/test_bitpool.py
+++ tests/test_bitpool.py
@@ -1,5 +1,6 @@
 import pytest
 import polars as pl
+import pandas as pd 
 from steganodf.algorithms.bitpool import BitPool
 
 from .utils import load_parameters
@@ -26,3 +27,17 @@ def test_with_password(df):
 	assert payload == algorithm.decode(df_encoded)
 
 
+@pytest.mark.parametrize("error_count", range(10))
+def test_with_error(error_count):
+
+	payload = b"hello"
+	algorithm = BitPool(password="password")
+	df_encoded = algorithm.encode(dfs[0].cast(pl.Utf8()), payload=payload)
+	df_pandas = df_encoded.to_pandas()
+
+	# Test with 10 errors 
+	for i in range(0,error_count):
+		df_pandas.iat[i,0] = "@"
+	
+	
+	assert payload == algorithm.decode(pl.from_pandas(df_pandas)), f"with error count = {i}"
