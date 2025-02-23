1c36c7facb2b5e341e3d3be51f88ac5cb3a791f8
sacha <sacha.schutz@pm.me>
2025-02-23 15:48:04 +0100

    wrong method

--- steganodf/__main__.py
+++ steganodf/__main__.py
@@ -15,53 +15,74 @@ SUPPORTED_FORMATS_IO = {
 
 def get_supported_input_format():
     return tuple(i for i, _ in SUPPORTED_FORMATS_IO if i and callable(i))
+
+
 def get_supported_output_format():
     return tuple(o for _, o in SUPPORTED_FORMATS_IO if o and callable(o))
 
 
-def read_file(path:Path) -> pl.DataFrame:
+def read_file(path: Path) -> pl.DataFrame:
     reader, _ = SUPPORTED_FORMATS_IO[path.suffix]
     return reader(path)
 
 
-def write_file(df: pl.DataFrame, path:Path):
+def write_file(df: pl.DataFrame, path: Path):
     _, writer = SUPPORTED_FORMATS_IO[path.suffix]
     writer(df, path)
-    
 
-    
+
 def ap_input_file(fname: str) -> Path:
     fname = Path(fname)
     if not os.path.exists(fname):
         raise argparse.ArgumentTypeError(f"{fname} does not exists")
     if not SUPPORTED_FORMATS_IO.get(fname.suffix, (None, None))[0]:
-        raise argparse.ArgumentTypeError(f"{fname} of format {fname.suffix} is not supported. Supported input file format are {', '.join(get_supported_input_format())}")
+        raise argparse.ArgumentTypeError(
+            f"{fname} of format {fname.suffix} is not supported. Supported input file format are {', '.join(get_supported_input_format())}"
+        )
     return Path(fname)
+
 
 def ap_output_file(fname: str) -> Path:
     fname = Path(fname)
     if not SUPPORTED_FORMATS_IO.get(fname.suffix, (None, None))[1]:
-        raise argparse.ArgumentTypeError(f"{fname} of format {fname.suffix} is not supported. Supported output file format are {', '.join(get_supported_output_format())}")
+        raise argparse.ArgumentTypeError(
+            f"{fname} of format {fname.suffix} is not supported. Supported output file format are {', '.join(get_supported_output_format())}"
+        )
     return Path(fname)
 
 
-def parse_cli(args = None) -> argparse.Namespace:
-    parser = argparse.ArgumentParser(prog='steganodf', description="a Tool to hide a message in a tabular file")
-    subparsers = parser.add_subparsers(dest='command', required=True)
+def parse_cli(args=None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="steganodf", description="a Tool to hide a message in a tabular file"
+    )
+    subparsers = parser.add_subparsers(dest="command", required=True)
 
     def add_common_args(subparser):
-        subparser.add_argument('input', type=ap_input_file, help="Input file")
-        subparser.add_argument('--password', '-p', type=str, required=False, help="Password to use")
-        subparser.add_argument('--algorithm', '-a', type=str, choices=list(ALGORITHMS.keys()), help="Algorithm to use", default="bitpool")
+        subparser.add_argument("input", type=ap_input_file, help="Input file")
+        subparser.add_argument("--password", "-p", type=str, required=False, help="Password to use")
+        subparser.add_argument(
+            "--algorithm",
+            "-a",
+            type=str,
+            choices=list(ALGORITHMS.keys()),
+            help="Algorithm to use",
+            default="bitpool",
+        )
 
     # command "encode"
-    encode_parser = subparsers.add_parser('encode', help="Encode given message in the input file data, write it to the output")
+    encode_parser = subparsers.add_parser(
+        "encode", help="Encode given message in the input file data, write it to the output"
+    )
     add_common_args(encode_parser)
-    encode_parser.add_argument('output', type=ap_output_file, help="File in which to write the data with message encoded in it")
-    encode_parser.add_argument('--message', '-m', type=str, required=True, help="Message to encode")
+    encode_parser.add_argument(
+        "output",
+        type=ap_output_file,
+        help="File in which to write the data with message encoded in it",
+    )
+    encode_parser.add_argument("--message", "-m", type=str, required=True, help="Message to encode")
 
     # command "decode"
-    decode_parser = subparsers.add_parser('decode', help="Decode a file with a hidden message")
+    decode_parser = subparsers.add_parser("decode", help="Decode a file with a hidden message")
     add_common_args(decode_parser)
 
     return parser.parse_args(args)
@@ -73,13 +94,15 @@ def main():
     if args.command == "encode":
 
         df = read_file(args.input)
-        new_df = st.encode_polars(df, payload=args.message.encode(), algorithm=args.algorithm, password=args.password)
+        new_df = st.encode(
+            df, payload=args.message.encode(), algorithm=args.algorithm, password=args.password
+        )
         write_file(new_df, args.output)
 
-    
     elif args.command == "decode":
         df = read_file(args.input)
-        print(st.decode_polars(df, algorithm=args.algorithm, password=args.password).decode())
+        print(st.decode(df, algorithm=args.algorithm, password=args.password).decode())
+
 
 if __name__ == "__main__":
     main()
